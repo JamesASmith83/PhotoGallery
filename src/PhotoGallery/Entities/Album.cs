868d9b744dd3385bb688e7cffcdce294b3ae991d
James Smith <james.a.smith.83@gmail.com>
2016-09-18 18:29:52 +0100

    Entity framework core added

---
+++ src/PhotoGallery/Entities/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhotoGallery.Entities
+{
+    public class Album : IEntityBase
+    {
+        public Album()
+        {
+            Photos = new List<Photo>();
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; }
+    }
+}
---
+++ src/PhotoGallery/Entities/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PhotoGallery.Entities
+{
+    public class Photo : IEntityBase
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Uri { get; set; }
+        public virtual Album Album { get; set; }
+        public int AlbumId { get; set; }
+        public DateTime DateUploaded { get; set; }
+    }
+}
