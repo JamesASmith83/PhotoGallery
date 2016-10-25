04ace4fb70461e214cd8b0605f41348dd1b7a680
James Smith <james.a.smith.83@gmail.com>
2016-10-25 20:25:41 +0100

    Finished membership service and started account controller

---
+++ src/PhotoGallery/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoGallery.Infrastructure.Repositories.Abstract;
+using PhotoGallery.Infrastructure.Services.Abstract;
+using PhotoGallery.ViewModels;
+using System.Threading.Tasks;
+
+namespace PhotoGallery.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IMembershipService _membershipService;
+        private readonly IUserRepository _userRepository;
+        private readonly ILoggingRepository _loggingRepository;
+
+        public AccountController(IMembershipService membershipService, IUserRepository userRepository, ILoggingRepository _errorRepository)
+        {
+            _membershipService = membershipService;
+            _userRepository = userRepository;
+            _loggingRepository = _errorRepository;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel user)
+        {
+
+        }
+
+
+
+
+    }
+}
