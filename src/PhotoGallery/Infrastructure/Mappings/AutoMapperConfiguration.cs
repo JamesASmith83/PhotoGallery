e5b47ad591e4cd8847cf40650b67a6666a488c66
James Smith <james.a.smith.83@gmail.com>
2016-11-08 14:50:15 +0000

    Create AutoMapperConfiguration.cs

---
+++ src/PhotoGallery/Infrastructure/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,10 @@
+public class AutoMapperConfiguration 
+{
+    public static void Configure()
+    {
+        Mapper.Initialize(x => 
+        {
+            x.AddProfile<DomainToViewModelMappingProfile>();
+        });
+    }
+}
